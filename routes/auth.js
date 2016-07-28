7f407c879c038303ad8e713a533421603fd50881
zhangbg <billandbiao@gmail.com>
2016-07-28 13:59:46 +0800

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -10,14 +10,14 @@ router.get('/', function(req, res, next) {
 
 //authentication by ldap
 // LDAP_HOST = '10.200.200.23'
-// LDAP_BASE_DN = 'OU=gridsum-members,DC=gridsum,DC=com'
-// LDAP_DOMAIN = 'gridsum.com'
+// LDAP_BASE_DN = 'OU=light-members,DC=light,DC=com'
+// LDAP_DOMAIN = 'light.com'
 router.get('/ldap', function(req, res) {
     var client = ldap.createClient({
-        url: 'ldap://10.200.200.23:389/OU=gridsum-members,DC=gridsum,DC=com'
+        url: 'ldap://10.200.200.23:389/OU=light-members,DC=light,DC=com'
     });
 
-    client.compare('OU=gridsum-members,DC=gridsum,DC=com', 'mail', 'zhangbiaoguang@gridsum.com', function(err, matched) {
+    client.compare('OU=light-members,DC=light,DC=com', 'mail', 'libill@light.com', function(err, matched) {
         console.log(err);
         console.log('matched: ' + matched);
         res.send(matched);
